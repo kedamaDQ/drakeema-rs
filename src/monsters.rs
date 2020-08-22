8aa08db064e9ccd32af7b89e81b89559f4d16038
kedama <kedama@foresdon.jp>
2020-08-22 17:54:34 +0900

    Add monsters for reaction (#22)

--- src/monsters.rs
+++ src/monsters.rs
@@ -6,18 +6,29 @@ use serde::Deserialize;
 use crate::{
 	Error,
 	Result,
-	resistances::{ Resistance, Resistances },
+	resistances::Resistances,
 	utils::transform_string_to_regex,
 };
+use crate::features::{ Reaction, ReactionCriteria };
 
+const DATA: &str = "data/contents/monsters.json";
 const DATA_DIR: &str = "data/monsters";
 
 pub struct Monsters {
+	information: String,
+	information_without_resistance: String,
+	area_names: HashMap<String, Vec<String>>,
+	ignore_categories: Vec<String>,
 	inner: HashMap<String, Monster>,
 }
 
 impl Monsters {
 	pub fn load() -> Result<Self> {
+		let monsters_json: MonstersJson = serde_json::from_reader(
+			BufReader::new(File::open(DATA)?)
+		)
+		.map_err(|e| Error::ParseJsonError(DATA.to_owned(), e))?;
+
 		let mut inner = HashMap::new();
 
 		let files = fs::read_dir(DATA_DIR)?
@@ -41,8 +52,40 @@ impl Monsters {
     	}
     
     	Ok(Monsters {
+			information: monsters_json.information,
+			information_without_resistance: monsters_json.information_without_resistance,
+			area_names: monsters_json.area_names,
+			ignore_categories: monsters_json.ignore_categories,
 			inner,
 		})
+	}
+}
+
+impl Reaction for Monsters {
+	fn reaction(&self, criteria: &ReactionCriteria) -> Option<String> {
+		let reaction = self.iter()
+			.filter(|(_, m)| {
+				!self.ignore_categories.contains(&m.category().to_owned()) &&
+				m.is_match(criteria.text())
+			})
+			.map(|(_, m)| {
+				let resistances = &m.resistances().display(self.area_names.get(m.category()));
+				if resistances.is_empty() {
+					self.information_without_resistance
+						.replace("__NAME__", m.official_name())
+				} else {
+					self.information
+						.replace("__NAME__", m.official_name())
+						.replace("__RESISTANCES__", &resistances)
+				}
+			})
+			.collect::<Vec<String>>();
+		
+		if reaction.is_empty() {
+			None
+		} else {
+			Some(reaction.join("\n"))
+		}
 	}
 }
 
@@ -56,17 +99,23 @@ impl std::ops::Deref for Monsters {
 #[derive(Debug, Clone, Deserialize)]
 pub struct Monster {
 	id: String,
+	category: String,
 	display: String,
 	official_name: String,
 	#[serde(deserialize_with="transform_string_to_regex")]
 	nickname_regex: Regex,
-	resistances: Resistances<Vec<Vec<Resistance>>>,
+	resistances: Resistances,
 }
 
 impl Monster {
 	/// Get an ID of this monster.
 	pub fn id(&self) -> &str {
 		self.id.as_ref()
+	}
+
+	/// Get the category of this monster.
+	pub fn category(&self) -> &str {
+		self.category.as_ref()
 	}
 
 	/// Get the common name of the monster displayed in the status.
@@ -85,11 +134,19 @@ impl Monster {
 	}
 
 	/// Get the resistances to need to battle the monster.
-	pub fn resistances(&self) -> &Resistances<Vec<Vec<Resistance>>> {
+	pub fn resistances(&self) -> &Resistances {
 		&self.resistances
 	}
 
 	pub fn is_match(&self, text: impl AsRef<str>) -> bool {
-		self.nickname_regex().is_match(text.as_ref())
+		self.nickname_regex.is_match(text.as_ref())
 	}
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct MonstersJson {
+	information: String,
+	information_without_resistance: String,
+	area_names: HashMap<String, Vec<String>>,
+	ignore_categories: Vec<String>,
 }
