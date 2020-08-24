cc75e150310c1b5a829a1abe038c753a57c27d4a
kedama <kedama@foresdon.jp>
2020-08-24 19:33:23 +0900

    Add weekly activity for announcement (#23)

---
+++ src/contents/weekly_activity.rs
@@ -0,0 +1,95 @@
+use std::fs::File;
+use std::io::BufReader;
+use chrono::{ Duration, Local, NaiveDateTime };
+use mastors::prelude::*;
+use mastors::api::v1::instance::activity;
+use serde::Deserialize;
+use crate::{
+	Error,
+	Result,
+	tmp_file,
+};
+use crate::features::{ Announcement, AnnouncementCriteria };
+
+const DATA: &str = "data/contents/weekly_activity.json";
+const TMP: &str = "weekly_activity.tmp";
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct WeeklyActivity {
+	announcement: String,
+}
+
+impl WeeklyActivity {
+	pub fn load() -> Result<Self> {
+		serde_json::from_reader(
+			BufReader::new(File::open(DATA)?)
+		)
+		.map_err(|e| Error::ParseJsonError(DATA.to_owned(), e))
+	}
+}
+
+impl Announcement for WeeklyActivity {
+	fn announcement(&self, _criteria: &AnnouncementCriteria) -> Option<String> {
+		use chrono::offset::TimeZone;
+
+		let conn = match Connection::from_file(crate::ENV_FILE) {
+			Ok(conn) => conn,
+			Err(e) => {
+				error!("Failed to get the connection: {}", e);
+				return None;
+			},
+		};
+
+		let latest_activity = match activity::get(&conn).send() {
+			Ok(activities) => {
+				match activities.get(1) {
+					Some(a) => *a,
+					None => return None,
+				}
+			},
+			Err(e) => {
+				error!("Failed to get the activity: {}", e);
+				return None;
+			},
+		};
+
+		let last_announced = match tmp_file::load_tmp_as_i64(TMP) {
+			Ok(opt) => match opt {
+				Some(data) => data,
+				None => 0,
+			},
+			Err(e) => {
+				error!("Failed to get last announced date: {}", e);
+				return None;
+			},
+		};
+
+		if latest_activity.week() <= last_announced {
+			return None;
+		}
+
+		let start_date = Local.from_utc_datetime(
+			&NaiveDateTime::from_timestamp(latest_activity.week(), 0)
+		).date();
+		let end_date = start_date + Duration::days(6);
+
+		if let Err(e) = tmp_file::save_tmp(
+			TMP, latest_activity.week().to_string()
+		) {
+			error!("Failed to write last announced date: {}", e);
+			return None;
+		}
+
+		Some(self.announcement
+			.replace("__START_DATE__", &start_date.format("%Y-%m-%d").to_string())
+			.replace("__END_DATE__", &end_date.format("%Y-%m-%d").to_string())
+			.replace("__ACTIVE_USER__", &latest_activity.logins().to_string())
+			.replace("__STATUS_COUNT__", &latest_activity.statuses().to_string())
+		)
+	}
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct WeeklyActivityJson {
+	announcement: String,
+}
---
+++ src/tmp_file.rs
@@ -0,0 +1,52 @@
+use std::fs;
+use std::path::Path;
+
+use crate::{
+	Error,
+	Result,
+};
+
+const TMP_PATH: &str = "data/tmp/";
+
+pub fn save_tmp(file: impl AsRef<str>, data: impl AsRef<str>) -> Result<()> {
+	let path = TMP_PATH.to_string() + file.as_ref();
+	let path = Path::new(&path);
+
+	fs::write(path, data.as_ref().trim())
+		.map_err(|e| Error::SaveTmpDataError(
+			path.to_string_lossy().to_string(), e
+		)
+	)
+}
+
+pub fn load_tmp_as_string(file: impl AsRef<str>) -> Result<Option<String>> {
+	let path = TMP_PATH.to_string() + file.as_ref();
+	let path = Path::new(&path);
+
+	if !path.exists() {
+		return Ok(None)
+	}
+
+	Ok(Some(
+		String::from_utf8_lossy(
+			&fs::read(path)
+				.map_err(|e|
+					Error::LoadTmpDataError(path.to_string_lossy().to_string(), e
+				)
+			)?
+		).trim().to_owned()
+	))
+}
+
+pub fn load_tmp_as_i64(file: impl AsRef<str>) -> Result<Option<i64>> {
+	match load_tmp_as_string(file) {
+		Ok(t) => match t {
+			Some(s) => Ok(Some(
+				i64::from_str_radix(&s, 10)
+					.map_err(|e| Error::TmpDataFormatError(s.to_owned(), e))?,
+			)),
+			None => Ok(None),
+		},
+		Err(e) => Err(e)
+	}
+}
