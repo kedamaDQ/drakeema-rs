c5684cb75cbb09d01b4d468c1951f767980e4fff
kedamaDQ <kedama@foresdon.jp>
2020-08-31 14:25:20 +0900

    Add logging

--- src/features/announcement_feed.rs
+++ src/features/announcement_feed.rs
@@ -1,7 +1,13 @@
 use std::fs::File;
 use std::io::BufReader;
+use std::thread;
+use std::time::Duration;
 use feed_rs::model::{
 	Entry,
+};
+use mastors::{
+	Connection,
+	api::v1::statuses,
 };
 use reqwest::blocking::{
 	Client,
@@ -14,18 +20,26 @@ use crate::{
 	tmp_file,
 	utils::transform_vec_string_to_vec_regex,
 };
+use super::{
+	AnnouncementCriteria,
+	Announcer,
+};
 
 const DATA: &str = "drakeema-data/announcement_feed.json";
 
 #[derive(Debug, Clone)]
-pub struct AnnouncementFeed {
+pub struct AnnouncementFeed<'a> {
+	conn: &'a Connection,
 	client: Client,
 	feed_urls: Vec<FeedUrl>,
 	title_regex: Vec<regex::Regex>,
+	post_interval_secs: u64,
 }
 
-impl AnnouncementFeed {
-	pub fn load() -> Result<Self> {
+impl<'a> AnnouncementFeed<'a> {
+	pub fn load(conn: &'a Connection) -> Result<Self> {
+		info!("Initializing AnnouncementFeed");
+
 		let json: AnnouncementFeedJson = serde_json::from_reader(
 			BufReader::new(File::open(DATA)?)
 		)
@@ -41,26 +55,46 @@ impl AnnouncementFeed {
 			.collect::<Vec<FeedUrl>>();
 
 		Ok(AnnouncementFeed {
+			conn,
 			client,
 			feed_urls,
 			title_regex: json.title_regex,
+			post_interval_secs: json.post_interval_secs,
 		})
 	}
 
-	pub fn get(&self) -> Result<Vec<String>> {
+	pub fn announce(&self, criteria: &AnnouncementCriteria) -> Result<()> {
+		info!("Start to announce feeds");
+		for text in self.get()? {
+			info!("Announce: {}", text.replace("\n", ""));
+			statuses::post(self.conn, text).send()?;
+			thread::sleep(Duration::from_secs(self.post_interval_secs));
+		}
+		Ok(())
+	}
+
+	fn get(&self) -> Result<Vec<String>> {
+		trace!("Start to get new feeds");
+
 		let mut entries = Vec::new();
 
 		for feed_url in self.feed_urls.iter() {
+			info!("Check {}", &feed_url.url);
+
 			let feed = feed_rs::parser::parse(
 				self.client.get(feed_url.url.clone()).send()?.text()?.as_bytes()
 			)
 			.map_err(|e| Error::ParseFeedError(feed_url.url.to_string(), e))?;
-			println!("{:#?}", feed);
 
+			trace!("Load last checked ID from {}", &feed_url.tmp);
 			let last_id = tmp_file::load_tmp_as_string(&feed_url.tmp)?;
+			trace!("Last checked ID: {:?}", last_id);
 
 			match self.search_new_items(&feed.entries, last_id) {
 				Some(items) => {
+					info!("Found {} new entries", items.len());
+					trace!("Write last checked ID to {}", &feed_url.tmp);
+
 					tmp_file::save_tmp(&feed_url.tmp, &items[0].id)?;
 					items
 				},
@@ -75,18 +109,18 @@ impl AnnouncementFeed {
 				})
 				.for_each(|e| entries.push(
 					format!(
-						"{}\n{}",
+						"{}\n\n{}",
 						&e.title.as_ref().unwrap().content,
 						&e.links.get(0).unwrap().href
 					)
 				));
 		}
 
-		println!("{:#?}", entries);
+		info!("Found {} new entries", entries.len());
 		Ok(entries)
 	}
 
-	fn search_new_items<'a>(&self, feed: &'a [Entry], id: Option<String>) -> Option<&'a [Entry]> {
+	fn search_new_items<'b>(&self, feed: &'b [Entry], id: Option<String>) -> Option<&'b [Entry]> {
 		let id = match id {
 			Some(id) => id,
 			None => return Some(feed),
@@ -110,14 +144,6 @@ impl AnnouncementFeed {
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-
-	#[test]
-	#[ignore]
-	fn test_test_test() {
-		let a = AnnouncementFeed::load().unwrap();
-		assert!(a.get().is_ok());
-	}
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -125,6 +151,7 @@ struct AnnouncementFeedJson {
 	feed_urls: Vec<Url>,
 	#[serde(deserialize_with = "transform_vec_string_to_vec_regex")]
 	title_regex: Vec<regex::Regex>,
+	post_interval_secs: u64,
 }
 
 #[derive(Debug, Clone)]
